a34d29b0ed10acce87e28a2a3a548272fb2b97f9
Sezgin Otlak <sezginotlakk@gmail.com>
2021-02-04 19:15:27 +0300

    First adjustments

---
+++ HighHeelClone/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    Vector3 newPosition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        newPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        newPosition = new Vector3(newPosition.x - 0.005f, transform.position.y, transform.position.z);
+        rigidbody.MovePosition(newPosition);
+    }
+}
---
+++ HighHeelClone/Assets/Scripts/TriggerHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerHandler : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            other.gameObject.GetComponent<BoxCollider>().enabled = false;
+            Destroy(gameObject, 0.6f);
+            Debug.Log("Triggered!");
+        }
+    }
+}
